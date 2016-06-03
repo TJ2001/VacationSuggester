471cd65dde62011c09eb5975d56445c5e5e8c22d
tj2001 <jung.m.tim@gmail.com>
2016-06-03 09:51:39 -0700

    it will use two preferences to provide location data

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,39 @@ $(document).ready(function() {
   $("#profile").submit(function(event) {
     event.preventDefault();
     var budget = $("#budget").val();
-    var budget = $("#budget").val();
+    var terrain = $("#terrain").val();
 
+
+    if (budget === 'student'){
     if (terrain === 'tropical') {
-      $('#muir').show();
+      $('#muirs').show();
+      $('#muir').hide();
       $('#bora').hide();
+      $('#boras').hide();
+    }
+
+    else if (terrain === 'forrest') {
+      $('#boras').hide();
+      $('#bora').hide();
+      $('#muir').hide();
+      $('#muirs').show();
+    }}
+
+    else if (budget === 'baller'){
+    if (terrain === 'tropical') {
+      $('#bora').show();
+      $('#boras').hide();
+      $('#muir').hide();
+      $('#muirs').hide();
     }
     else if (terrain === 'forrest') {
-      $('#bora').show();
-      $('#muir').hide();
-    }
+      $('#boras').hide();
+      $('#bora').hide();
+      $('#muir').show();
+      $('#muirs').hide();
+    }}
+
+
     else {
       console.log("wrong");
     }
