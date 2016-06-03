7e6ac64aa5713669eb82e89b3c7799cf52240924
tj2001 <jung.m.tim@gmail.com>
2016-06-03 09:18:27 -0700

    added preference1 and worked on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,21 @@
+$(document).ready(function() {
+  $("#profile").submit(function(event) {
+    event.preventDefault();
+    var budget = $("#budget").val();
+    var budget = $("#budget").val();
+
+    if (terrain === 'tropical') {
+      $('#muir').show();
+      $('#bora').hide();
+    }
+    else if (terrain === 'forrest') {
+      $('#bora').show();
+      $('#muir').hide();
+    }
+    else {
+      console.log("wrong");
+    }
 
 
-$("#profile").submit(function(event) {
-  event.preventDefault();
-      debugger;
-
-
-
-// Question 3
-          var ls = $("#landscape").val();
-
-          if (ls === 'mountain') {
-            alert("i'm working!");
-          }
-          else if (sign === 'airies' || sign === 'taurus') {
-            $('#george').show();
-          }
-          else if (sign === 'capricorn' || sign === 'sagittarius') {
-            $('#taylor').show();
-          }
-          else if (sign === 'gemini' || sign === 'scorpio') {
-            $('#steve').show();
-          }
-          else if (sign === 'libra' || sign === 'leo') {
-            $('#louis').show();
-          }
-          else if (sign === 'auquarius' || sign === 'pisces') {
-            $('#sean').show();
-          }
-
-          else {
-            console.log("wrong");
-          }
-
-        });
+  });
+});
