376339faf5d6ac8a5c6ba8b45cbae8c94100dcae
tj2001 <jung.m.tim@gmail.com>
2016-06-03 14:45:21 -0700

    it will have the third preference working

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,197 @@ $(document).ready(function() {
     event.preventDefault();
     var budget = $("#budget").val();
     var terrain = $("#terrain").val();
+    var activity = $("#activity").val();
+    // debugger;
 
+    if (activity === 'community') {
+      if (budget === 'student'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').show();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
 
-    if (budget === 'student'){
-    if (terrain === 'tropical') {
-      $('#muirs').show();
-      $('#muir').hide();
-      $('#bora').hide();
-      $('#boras').hide();
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').show();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }}
+
+      else if (budget === 'baller'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').show();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').show();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }}};
+
+    if (activity === 'romantic') {
+      if (budget === 'student'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').show();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').show();
+        $('#muirs3').hide();
+      }}
+      else if (budget === 'baller'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').show();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').show();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }}
     }
 
-    else if (terrain === 'forrest') {
-      $('#boras').hide();
-      $('#bora').hide();
-      $('#muir').hide();
-      $('#muirs').show();
-    }}
-
-    else if (budget === 'baller'){
-    if (terrain === 'tropical') {
-      $('#bora').show();
-      $('#boras').hide();
-      $('#muir').hide();
-      $('#muirs').hide();
+      if (activity === 'markets') {
+      if (budget === 'student'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').show();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').show();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }}
+      else if (budget === 'baller'){
+      if (terrain === 'tropical') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').show();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').hide();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }
+      else if (terrain === 'forrest') {
+        $('#bora').hide();
+        $('#bora2').hide();
+        $('#bora3').hide();
+        $('#boras').hide();
+        $('#boras2').hide();
+        $('#boras3').hide();
+        $('#muir').hide();
+        $('#muir2').hide();
+        $('#muir3').show();
+        $('#muirs').hide();
+        $('#muirs2').hide();
+        $('#muirs3').hide();
+      }}
     }
-    else if (terrain === 'forrest') {
-      $('#boras').hide();
-      $('#bora').hide();
-      $('#muir').show();
-      $('#muirs').hide();
-    }}
 
-
-    else {
-      console.log("wrong");
-    }
-
+    //
+    //
+    // else {console.log("wrong");
+    // }
 
   });
 });
